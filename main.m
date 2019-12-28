80fc2c00253a531dd998e70071b77533f3137d2f
SnowzTail <i@snowztail.com>
2019-12-28 18:13:20 +0000

    :construction: R-E region (in progress)

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 initialize; config;
 
+%% R-E region calculation
 nAngles = size(channelRelativeAngle, 2);
 nWeights = size(weight, 2);
 
@@ -19,4 +20,32 @@ for iAngle = 1 : nAngles
         [slrsRate{iAngle, iWeight}] = slrs_rate(weight(:, iWeight), bcChannel, snr, tolerance, rsRatio);
         [rsRate{iAngle, iWeight}] = rs_rate(weight(:, iWeight), bcChannel, snr, tolerance, rsRatio);
     end
+end
+
+%% R-E region comparison
+figure('Name', sprintf('Achievable rate region comparison for %d-user %d-tx deployment, with \gamma = %d and SNR = %d dB', [user, rx, channelRelativeStrength, snr]));
+legendString = cell(nAngles, 1);
+for iAngle = 1 : nAngles
+    subplot(2, 2, iAngle);
+    % DPC
+    dpcPlot = plot(dpcRate{iAngle, :});
+    legendString{1} = sprintf('DPC');
+
+    % MU-LP
+    mulpPlot = plot(mulpRate{iAngle, :});
+    legendString{2} = sprintf('MU-LP');
+
+    % NOMA
+    nomaIdx = convhull(nomaRate{iAngle, :});
+    nomaPlot = plot(sortrow(nomaRate{iAngle, nomaIdx}));
+    legendString{3} = sprintf('NOMA');
+
+    % Single-layer RS
+    slrsPlot = plot(slrsRate{iAngle, :});
+    legendString{4} = sprintf('SLRS');
+
+    % RS
+    rsIdx = convhull(rsRate{iAngle, :});
+    rsPlot = plot(sortrow(rsRate{iAngle, rsIdx}));
+    legendString{5} = sprintf('RS');
 end
